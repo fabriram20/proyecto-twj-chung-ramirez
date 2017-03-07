5d061df1a7898fdc742ed03e7a7a55651b630078
fabriram20 <fabriram20@gmail.com>
2017-03-07 03:48:15 -0500

    Vista home + categoria + producto

--- TiendaDeRopaFrontEnd/src/app/home/home.component.ts
+++ TiendaDeRopaFrontEnd/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Http, Response} from "@angular/http";
+import {MasterUrlService} from "../services/master-url.service";
+import {NgForm} from "@angular/forms";
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private _parametros:any;
+  productos = [];
+  categorias = [];
 
-  ngOnInit() {
+
+  constructor(private _activatedRoute: ActivatedRoute, private _http: Http, private _masterURL: MasterUrlService) {
   }
 
+  ngOnInit() {
+    this._activatedRoute
+      .params
+      .subscribe(parametros => {
+        this._parametros = parametros;
+
+        // Peticion de productos
+        this._http.get(this._masterURL.url + "Producto").subscribe(
+          (res: Response) => {
+            this.productos = res.json().map((value) => {
+              value.formularioCerrado = true;
+              return value;
+            });
+            ;
+          },
+          (err) => {
+            console.log(err);
+          }
+        )
+        // Peticion de categorias
+        this._http.get(this._masterURL.url + "Categoria").subscribe(
+          (res: Response) => {
+            this.categorias = res.json()
+              .map((value) => {
+                value.idformularioCerrado = true;
+                return value;
+              });
+          },
+          (err) => {
+            console.log(err);
+          }
+        )
+      })
+  }
 }
