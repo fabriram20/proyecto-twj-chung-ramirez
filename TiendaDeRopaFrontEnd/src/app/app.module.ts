3c2be83d03c6c6833308f3cfd7830d3444b9d335
fabriram20 <fabriram20@gmail.com>
2017-03-06 22:04:06 -0500

    Componente Home Producto y Categoria

--- TiendaDeRopaFrontEnd/src/app/app.module.ts
+++ TiendaDeRopaFrontEnd/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductoComponent } from './producto/producto.component';
+import { CategoriaComponent } from './categoria/categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductoComponent,
+    CategoriaComponent
   ],
   imports: [
     BrowserModule,
