0dca20339519a9757c6caaa94bb34d12e088e095
chung-edison <eddy2809@hotmail.com>
2017-03-06 20:27:49 -0500

    APIs y configuracion BackEnd

---
+++ TiendaDeRopa/api/controllers/DetalleController.js
@@ -0,0 +1,11 @@
+/**
+ * DetalleController
+ *
+ * @description :: Server-side logic for managing Detalles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ TiendaDeRopa/api/models/Producto.js
@@ -0,0 +1,32 @@
+/**
+ * Producto.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombreProducto: {
+      type: string,
+      required: true
+    },
+    foto: {
+      type: string,
+      defaultsTo: "blank.jpg"
+    },
+    precio: {
+      type: number,
+      required: true
+    },
+    idCategoria: {
+      model: "Categoria"
+    },
+    detalles: {
+      collection: "Detalle",
+      via: "idProducto"
+    }
+  }
+};
+
